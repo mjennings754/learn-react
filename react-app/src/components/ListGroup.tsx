6187ce3675e2d8163035588a01df797901ae8c27
mjennings754 <mitchellj@outlook.jp>
2026-03-10 21:48:05 +1100

    rendering lists

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,20 @@
+function ListGroup() {
+    const items = [
+        'New york',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+    return (
+    <>
+    <h1>List</h1>
+    <ul className="list-group">
+        {items.map(item => <li key={item}>{item}</li>)}
+    </ul>
+    </>
+    );
+}
+
+export default ListGroup;
