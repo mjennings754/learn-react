4601203ba460310d5275d73c7033c8dc10920f7f
mjennings754 <mitchellj@outlook.jp>
2026-03-10 22:00:52 +1100

    handling events

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,19 +1,19 @@
+import { MouseEvent } from "react";
 function ListGroup() {
-    let items = [
+    const items = [
         'New york',
         'San Francisco',
         'Tokyo',
         'London',
         'Paris'
     ];
-    items = [];
-
+    const handleClick = (event : MouseEvent) => console.log(event);
     return (
     <>
     <h1>List</h1>
     {items.length === 0 && <p>No item found</p>}
     <ul className="list-group">
-        {items.map(item => <li key={item}>{item}</li>)}
+        {items.map((item) => <li className="list-group-item" key={item} onClick={handleClick}>{item}</li>)}
     </ul>
     </>
     );
